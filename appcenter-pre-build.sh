688e5cb274082721c38105579cd9bdfaf63e686f
Krishan Saini <krishan.saini@deorwine.com>
2019-12-21 14:34:22 +0530

    testing

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # Creates an .env and local.properties from ENV variables for use with react-native-config
 
-ENV_WHITELIST=${ENV_WHITELIST:-"^RN_"}
-printf "Creating an .env file with the following whitelist:\n"
-printf "%s\n" $ENV_WHITELIST
-set | egrep -e $ENV_WHITELIST | sed 's/^RN_//g' > .env
-printf "\n.env created with contents:\n\n"
-cat .env
+# ENV_WHITELIST=${ENV_WHITELIST:-"^RN_"}
+# printf "Creating an .env file with the following whitelist:\n"
+# printf "%s\n" $ENV_WHITELIST
+# set | egrep -e $ENV_WHITELIST | sed 's/^RN_//g' > .env
+# printf "\n.env created with contents:\n\n"
+# cat .env
 
 ENV_WHITELIST=${ENV_WHITELIST:-"^AN_"}
 printf "Creating an local.properties file with the following whitelist:\n"
